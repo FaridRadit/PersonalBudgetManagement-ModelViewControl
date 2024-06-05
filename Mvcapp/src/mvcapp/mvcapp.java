143cf241c49ee53731dd94e2db61cbde06420c42
FaridRadit <162663946+FaridRadit@users.noreply.github.com>
2024-06-05 20:33:47 +0700

    Delete Mvcapp directory

--- Mvcapp/src/mvcapp/mvcapp.java
+++
@@ -1,9 +0,0 @@
-package mvcapp;
-
-public class mvcapp {
-    public static void main(String[] args) {
-        ModelContact model = new ModelContact();
-        ViewContact view = new ViewContact();
-        ControllerContact controller = new ControllerContact(model, view);
-    }
-}
